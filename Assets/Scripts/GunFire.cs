9254e5b2a06c1af6bafcb0f3109f3904d102038e
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2023-02-21 22:59:00 +0300

    v 0.47

--- Assets/Scripts/GunFire.cs
+++ Assets/Scripts/GunFire.cs
@@ -30,15 +30,15 @@ public class GunFire : MonoSingleton<GunFire>
         MainManager mainManager = MainManager.Instance;
 
         yield return null;
-        tempQuaternion = mainManager.allGuns.Guns[mainManager.gunCount].transform.rotation;
+        tempQuaternion = mainManager.allGuns[0].Guns[mainManager.gunCount].transform.rotation;
 
         while (gameManager.gameStat == GameManager.GameStat.start)
         {
             yield return null;
 
-            for (int i = 0; i < mainManager.allGuns.GunIDs[mainManager.gunCount].hitStartPos.Count; i++)
+            for (int i = 0; i < mainManager.allGuns[0].GunIDs[mainManager.gunCount].hitStartPos.Count; i++)
                 if (walkerManager.Walker.Count > 0 && RotateOnTouch.Instance.canRotate)
-                    ManuelHitRival(mainManager.allGuns.GunIDs[mainManager.gunCount].hitStartPos[i], mainManager, itemData);
+                    ManuelHitRival(mainManager.allGuns[0].GunIDs[mainManager.gunCount].hitStartPos[i], mainManager, itemData);
 
             yield return new WaitForSeconds(itemData.field.gunReloadTime);
         }
@@ -53,7 +53,7 @@ public class GunFire : MonoSingleton<GunFire>
 
         bool isRivalSee = false;
         int tempRivalCount = -1;
-        tempQuaternion = mainManager.allGuns.Guns[mainManager.gunCount].transform.rotation;
+        tempQuaternion = mainManager.allGuns[gunCount].Guns[mainManager.gunCount].transform.rotation;
 
         yield return null;
 
@@ -62,22 +62,22 @@ public class GunFire : MonoSingleton<GunFire>
             yield return null;
 
             for (int i = 0; i < walkerManager.Walker.Count; i++)
-                if (itemData.field.gunDistance > Vector3.Distance(walkerManager.Walker[i].transform.position, mainManager.allGuns.GunIDs[mainManager.gunCount].mainCharacter.transform.position))
+                if (itemData.field.gunDistance > Vector3.Distance(walkerManager.Walker[i].transform.position, mainManager.allGuns[gunCount].GunIDs[mainManager.gunCount].mainCharacter.transform.position))
                     if (isRivalSee)
                     {
-                        if (Vector3.Distance(walkerManager.Walker[tempRivalCount].transform.position, mainManager.allGuns.GunIDs[mainManager.gunCount].mainCharacter.transform.position) > Vector3.Distance(walkerManager.Walker[i].transform.position, mainManager.allGuns.GunIDs[mainManager.gunCount].mainCharacter.transform.position))
+                        if (Vector3.Distance(walkerManager.Walker[tempRivalCount].transform.position, mainManager.allGuns[gunCount].GunIDs[mainManager.gunCount].mainCharacter.transform.position) > Vector3.Distance(walkerManager.Walker[i].transform.position, mainManager.allGuns[gunCount].GunIDs[mainManager.gunCount].mainCharacter.transform.position))
                             RivalSeeTrue(ref isRivalSee, ref tempRivalCount, i);
                     }
                     else
                         RivalSeeTrue(ref isRivalSee, ref tempRivalCount, i);
 
-            /* if (isRivalSee)
-             {
-                 foreach (GameObject pos in mainManager.allGuns.GunIDs[mainManager.gunCount].hitStartPos)
-                     HitRival(walkerManager.Walker[tempRivalCount], pos, mainManager, itemData, gunCount);
-                 yield return new WaitForSeconds(itemData.field.gunReloadTime);
-                 RivalSeeFalse(ref isRivalSee, ref tempRivalCount);
-             }*/
+            if (isRivalSee)
+            {
+                foreach (GameObject pos in mainManager.allGuns[gunCount].GunIDs[mainManager.gunCount].hitStartPos)
+                    HitRival(walkerManager.Walker[tempRivalCount], pos, mainManager, itemData, gunCount);
+                yield return new WaitForSeconds(itemData.field.gunReloadTime);
+                RivalSeeFalse(ref isRivalSee, ref tempRivalCount);
+            }
 
             /*if (gameManager.level % WalkerManager.Instance.bossModLevel != 0)
             {
@@ -114,18 +114,16 @@ public class GunFire : MonoSingleton<GunFire>
         StartCoroutine(HitMoveRigidbody(hit));
 
     }
-    /* private void HitRival(GameObject focusRival, GameObject pos, MainManager mainManager, ItemData itemData, int gunCount)
-     {
-         if (focusRival.activeInHierarchy)
-         {
-             GameObject hit = GetObject(mainManager);
-             HitPlacement(ref hit, pos);
-             GunEffect(hit);
-             GunLookFocusRival(focusRival, mainManager, gunCount);
-             StartCoroutine(GunShake(mainManager, itemData, gunCount));
-             StartCoroutine(HitMove(hit, focusRival));
-         }
-     }*/
+    private void HitRival(GameObject focusRival, GameObject pos, MainManager mainManager, ItemData itemData, int gunCount)
+    {
+        if (focusRival.activeInHierarchy)
+        {
+            GameObject hit = GetObject(mainManager);
+            HitPlacement(ref hit, pos);
+            GunEffect(hit);
+            StartCoroutine(HitMove(hit, focusRival));
+        }
+    }
     private GameObject GetObject(MainManager mainManager)
     {
         return ObjectPool.Instance.GetPooledObject(OPHitCount + mainManager.gunCount);
@@ -141,18 +139,18 @@ public class GunFire : MonoSingleton<GunFire>
     }
     private void GunLookFocusRival(GameObject focusRival, MainManager mainManager)
     {
-        mainManager.allGuns.Guns[mainManager.gunCount].transform.LookAt(focusRival.transform);
-        mainManager.allGuns.Guns[mainManager.gunCount].transform.rotation = new Quaternion(tempQuaternion.x, mainManager.allGuns.Guns[mainManager.gunCount].transform.rotation.y, tempQuaternion.z, mainManager.allGuns.Guns[mainManager.gunCount].transform.rotation.w);
+        mainManager.allGuns[0].Guns[mainManager.gunCount].transform.LookAt(focusRival.transform);
+        mainManager.allGuns[0].Guns[mainManager.gunCount].transform.rotation = new Quaternion(tempQuaternion.x, mainManager.allGuns[0].Guns[mainManager.gunCount].transform.rotation.y, tempQuaternion.z, mainManager.allGuns[0].Guns[mainManager.gunCount].transform.rotation.w);
     }
     private IEnumerator GunShake(MainManager mainManager, ItemData itemData)
     {
-        mainManager.allGuns.Guns[mainManager.gunCount].transform.DOShakeScale(((itemData.field.gunReloadTime) / 3) * 2, _gunScalePower);
+        mainManager.allGuns[0].Guns[mainManager.gunCount].transform.DOShakeScale(((itemData.field.gunReloadTime) / 3) * 2, _gunScalePower);
         yield return new WaitForSeconds(itemData.field.gunReloadTime);
-        mainManager.allGuns.Guns[mainManager.gunCount].transform.localScale = new Vector3(_gunScale, _gunScale, _gunScale);
+        mainManager.allGuns[0].Guns[mainManager.gunCount].transform.localScale = new Vector3(_gunScale, _gunScale, _gunScale);
     }
     private IEnumerator HitMoveRigidbody(GameObject hit)
     {
-        float xangle = MainManager.Instance.allGuns.ParentGun.transform.GetChild(MainManager.Instance.gunCount).transform.GetChild(0).transform.rotation.eulerAngles.y;
+        float xangle = MainManager.Instance.allGuns[0].ParentGun.transform.GetChild(MainManager.Instance.gunCount).transform.GetChild(0).transform.rotation.eulerAngles.y;
         if (xangle > 300) xangle -= 360;
         xangle /= 6;
         hit.GetComponent<Rigidbody>().velocity = new Vector3(xangle, 0, _hitVelocityPower);
